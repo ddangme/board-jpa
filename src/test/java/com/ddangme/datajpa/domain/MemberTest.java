c2e288e13d4e6cc8df0948f234457fcbc46642bc
ddangme <ssilver121@naver.com>
2024-01-19 13:16:41 +0900

    #2 - 도메인 모델과 동작 확인

---
+++ src/test/java/com/ddangme/datajpa/domain/MemberTest.java
@@ -0,0 +1,55 @@
+package com.ddangme.datajpa.domain;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void testEntity() {
+        // Given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        // When
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+
+        // Then
+        List<Member> members = em.createQuery("SELECT m FROM Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.print(member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+    }
+}
